3c3628e4782ffd436ae20c7455e7bf9fb6036833
Leandro-Valora <leonardolevati1998@gmail.com>
2024-02-28 14:27:53 +0100

    secondo commit

--- src/admin/AdBar.js
+++ src/admin/AdBar.js
@@ -3,44 +3,87 @@ import './AdBar.css';
 import LatWindow from './LatWindow';
 import { Link } from 'react-router-dom';
 
+// function AdBar() {
+//     return (
+//         <>
+//             <nav className="AdBar">
+//                 <div className="row-no-gutters">
+//                     <a className="AdBar-brand" href="https://realhome1.netlify.app/">
+//                         <img src={logo} alt="Logo" width="40" height="40" />
+//                     </a>
+//                     <div className="header-1">
+//                         <ul className="nav nav-tabs">
+//                             <li className="nav-item">
+//                                 <Link to="/admin/listaAdmin" className="btn btn-outline-primary"> Lista Admin </Link>
+//                             </li>
+//                             <li className="nav-item">
+//                             <Link to="/admin/listaUser" className="btn btn-outline-primary"> Lista User </Link>
+//                             </li>
+//                             <li className="nav-item">
+//                             <Link to="/admin/listaCase" className="btn btn-outline-primary"> Lista Case </Link>
+//                             </li>
+//                         </ul>
+//                     </div>
+//                     <br />
+//                     <div className="header-2">
+//                         <form className="d-flex" role="search">
+//                             <input className="form-control me-2" type="search" placeholder="Citta'" aria-label="Search" />
+//                             <p></p>
+//                             <input className="form-control me-2" type="search" placeholder="indirizzo" aria-label="Search" />
+//                             <p></p>
+//                             <input className="form-control me-2" type="search" placeholder="range di prezzo" aria-label="Search" />
+//                             <button className="btn btn-outline-success" type="submit">
+//                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+//                                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+//                                 </svg>
+//                             </button>
+//                         </form>
+//                     </div>      
+//                 </div>
+//             </nav>
+//             <LatWindow />
+//         </>
+//     );
+// }
+
+// export default AdBar;
+
 function AdBar() {
     return (
         <>
-            <bar className="AdBar">
+            <nav className="AdBar">
                 <div className="row-no-gutters">
                     <a className="AdBar-brand" href="https://realhome1.netlify.app/">
                         <img src={logo} alt="Logo" width="40" height="40" />
                     </a>
                     <div className="header-1">
-                        <ul className="bar bar-tabs">
-                            <li className="bar-item">
+                        <ul className="nav nav-tabs">
+                            <li className="nav-item">
                                 <Link to="/admin/listaAdmin" className="btn btn-outline-primary"> Lista Admin </Link>
                             </li>
-                            <li className="bar-item">
-                            <Link to="/admin/listaUser" className="btn btn-outline-primary"> Lista User </Link>
+                            <li className="nav-item">
+                                <Link to="/admin/listaUser" className="btn btn-outline-primary"> Lista User </Link>
                             </li>
-                            <li className="bar-item">
-                            <Link to="/admin/listaCase" className="btn btn-outline-primary"> Lista Case </Link>
+                            <li className="nav-item">
+                                <Link to="/admin/listaCase" className="btn btn-outline-primary"> Lista Case </Link>
                             </li>
                         </ul>
                     </div>
                     <br />
                     <div className="header-2">
-                        <form className="d-flex" role="search">
+                        <div className="search-inputs"> {/* Nuovo div per gli input di ricerca */}
                             <input className="form-control me-2" type="search" placeholder="Citta'" aria-label="Search" />
-                            <p></p>
                             <input className="form-control me-2" type="search" placeholder="indirizzo" aria-label="Search" />
-                            <p></p>
                             <input className="form-control me-2" type="search" placeholder="range di prezzo" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                                </svg>
-                            </button>
-                        </form>
+                        </div>
+                        <button className="btn btn-outline-success" type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                            </svg>
+                        </button>
                     </div>      
                 </div>
-            </bar>
+            </nav>
             <LatWindow />
         </>
     );
--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,77 +1,3 @@
-// import React from 'react';
-// import AdBar from './AdBar';
-// import Footer from '../components/Footer';
-// import UserProfile from '../UserProfile';
-// import "./Admin.css";
-// import villa1 from "../ville/avi.jpg";
-// import villa2 from "../ville/dinuka.jpg";
-// import villa3 from "../ville/fern.jpg";
-
-// function Admin() {
-//     state = {
-//         footers: [
-//             { id: 0, indirizzo: 'Parma, PR 43122, IT', email: 'infoaboutRH@gmail.com', telefono: '+39 0375 833639', cellulare: '+39 345 6139884', brand: 'Real - Home'}        
-//         ]
-//     }
-
-//     //check se è loggato
-//     componentDidMount() {
-//         const userName = UserProfile.getName();
-//         if (!userName || userName.trim() === "") {
-//             // Reindirizza l'utente alla pagina principale se il nome è vuoto
-//             window.location.href = "/";
-//         }
-//     }
-
-//     return (
-//         <>
-//             <AdBar />
-//             <br />     
-//             {/* QUI CODICE per pagina specifica  */}
-            
-//             <div>
-//                 <header>
-//                     <h1 className='titolo-1'><center>Benvenuti all'Agenzia Immobiliare Real-Home</center></h1>
-//                 </header>
-//                 <br />
-//                 <main>
-//                     <section>
-//                         <h2>Ultime Proprietà Vendute </h2>
-//                         <div className="property-gallery">
-//                             <div className="property">
-//                                 <img src={villa1} alt="Property 1" />
-//                                 <h3>Villa marea</h3>
-//                                 <p>grande dimora, di proprietà del signor berlusconi</p>
-//                                 <p><b>Prezzo</b> : &euro; 5.500.000</p>
-//                             </div>
-//                             <div className="property">
-//                                 <img src={villa2} alt="Property 2" />
-//                                 <h3>Villa arcore</h3>
-//                                 <p>Descrizione della proprietà</p>
-//                                 <p><b>Prezzo</b> : &euro; 5.500.000</p>
-//                             </div>
-//                             <div className="property">
-//                                 <img src={villa3} alt="Property 3" />
-//                                 <h3>Villetta schiera New York</h3>
-//                                 <p>Descrizione della proprietà</p>
-//                                 <p><b>Prezzo</b> : &euro; 10.500.000</p>
-//                             </div>
-//                         </div>
-//                     </section>
-//                 </main>
-//             </div>
-//             {/* FINE */}
-//             {this.state.footers.map(footer => (
-//                 <Footer
-//                     key={footer.id}
-//                     footer={footer} />
-//             ))}
-//         </>
-//     );
-// }
-
-// export default Admin;
-
 import React from 'react';
 import AdBar from './AdBar';
 import Footer from '../components/Footer';
@@ -100,9 +26,7 @@ class Admin extends React.Component {
         return (
             <>
                 <AdBar />
-                <br />     
-                {/* QUI CODICE per pagina specifica */}
-                
+                <br />
                 <div>
                     <header>
                         <h1 className='titolo-1'><center> Nuovo Benvenuti all'Agenzia Immobiliare Real-Home</center></h1>
